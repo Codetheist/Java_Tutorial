c7ba80384993d9b9b85e8e090315d2dfaf9e8236
Codetheist <56620151+Codetheist@users.noreply.github.com>
2021-01-12 10:09:57 -0500

    I didn't close the scanner variable.

--- COP2800_Java_Programming/Module 1/src/H1_PizzaShop.java
+++ COP2800_Java_Programming/Module 1/src/H1_PizzaShop.java
@@ -1,3 +1,4 @@
+
 /****************************************************************************************************
  Homework 1: Malachi’s Pizza Shop
  Program By: Antoine Gustave
@@ -18,17 +19,23 @@ public class H1_PizzaShop {
         Scanner userInput = new Scanner(System.in);
 
         System.out.println("Welcome to Malachi’s Pizza Shop!");
-        System.out.println("Please enter the DIAMETER of your pizza (inches): ");
-        pizzaDiameter = userInput.nextInt();
-        System.out.println("Please enter the Area of your desired pizza slice (inches): ");
-        sliceArea = userInput.nextDouble();
-        System.out.println();
+        try {
+
+            System.out.println("Please enter the DIAMETER of your pizza (inches): ");
+            pizzaDiameter = userInput.nextInt();
+            System.out.println("Please enter the Area of your desired pizza slice (inches): ");
+            sliceArea = userInput.nextDouble();
+            System.out.println();
+        } finally {
+            userInput.close();
+        }
 
         double pizzaRadius = pizzaDiameter / 2;
-        double pizzaArea = PI * Math.pow(pizzaRadius,2);
+        double pizzaArea = PI * Math.pow(pizzaRadius, 2);
         double numberOfSlices = pizzaArea / sliceArea;
 
         System.out.println("Your pizza has an AREA of " + pizzaArea + " inches sq.");
-        System.out.println("You can evenly divide your pizza into " + (int) numberOfSlices + " slices based on your slice area of " + sliceArea + " inches sq.");
+        System.out.println("You can evenly divide your pizza into " + (int) numberOfSlices
+                + " slices based on your slice area of " + sliceArea + " inches sq.");
     }
 }
