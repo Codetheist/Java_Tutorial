4eceefede76dadaeada321bbe815a23727f750c8
Codetheist <56620151+Codetheist@users.noreply.github.com>
2021-02-08 18:45:58 -0500

    Created a selection program.

---
+++ COP2800_Java_Programming/src/Module 3/Example/Selections.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Selections {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        /*
+         * String name = scan.nextLine(); int age = scan.nextInt();
+         * 
+         * System.out.println(name); System.out.println(age);
+         */
+
+        /*
+         * int num = 0; System.out.println("Enter a num: "); num = scan.nextInt();
+         * 
+         * if (num % 2 == 0) { System.out.println("Even"); } else {
+         * System.out.println("Odd"); }
+         */
+
+        /*
+         * int grade = 90;
+         * 
+         * if (grade >= 90) { // } else if (grade >= 80) { // }else if (grade >= 70) {
+         * // } else if (grade >= 60) { // }else if (grade >= 0) { // } else { // }
+         */
+
+        /*int degree = 0;
+        int experience = 0;
+
+        System.out.println("Do you have a degree (1. For Yes or 2. For No): ");
+        degree = scan.nextInt();
+        System.out.println("How many years of experience: ");
+        experience = scan.nextInt();
+
+        if (degree == 1 && experience >= 4) {
+        
+            System.out.println("You're hired!");
+        
+        } else if (degree == 2 && experience < 0) {
+        
+            System.out.println("Come back when you have experience");
+        
+        } else {
+        
+            System.out.println("Not hired!");
+        }*/
+    }
+}
