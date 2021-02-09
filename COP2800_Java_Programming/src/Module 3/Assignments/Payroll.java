46b8f85d80155d6741ce28a2f7d01a58157fc29f
Codetheist <56620151+Codetheist@users.noreply.github.com>
2021-02-09 16:23:35 -0500

    User input and calculation fixed.

--- COP2800_Java_Programming/src/Module 3/Assignments/Payroll.java
+++ COP2800_Java_Programming/src/Module 3/Assignments/Payroll.java
@@ -9,54 +9,58 @@ import java.util.Scanner;
 public class Payroll {
 
     public static void main(String[] args) {
+        System.out.println("Welcome to Payroll!");
+        System.out.println("Please enter the number of employees: ");
         bonus();
     }
 
     public static void bonus() {
         Scanner userInput = new Scanner(System.in);
-        double totalEmployees = 0.0;
-        double employeesYears = 0.0;
+        int totalEmployees = 0;
+        int employeesYears = 0;
         double bonus = 0.0;
         double currentPay = 0.0;
         double newPay = 0.0;
 
-        for (int i = 0; i < totalEmployees; i++) {
-            if (employeesYears == 0 && employeesYears <= 4) {
-                bonus = .05;
-                bonus = currentPay * bonus;
-                newPay = currentPay + bonus;
-            } else if (employeesYears == 5 && employeesYears <= 9) {
-                bonus = .10;
-                bonus = currentPay * bonus;
-                newPay = currentPay + bonus;
-            } else if (employeesYears >= 10) {
-                bonus = .15;
-                bonus = currentPay * bonus;
-                newPay = currentPay + bonus;
-            } else {
-                System.out.println("Invalid input.");
-            }
-        }
-
         try {
-            System.out.println("Welcome to Payroll!");
-            System.out.println("Please enter the number of employees: ");
-            totalEmployees = userInput.nextDouble();
-            System.out.println("Please enter employee 1’s monthly pay: ");
-            currentPay = userInput.nextDouble();
-            System.out.println("Please enter the number of years employee 1 has worked: ");
-            employeesYears = userInput.nextDouble();
-            System.out.println("Employee one receives a 5 % bonus, bringing their monthly pay to $" + newPay);
-            System.out.println("Please enter employee 2’s monthly pay: ");
-            currentPay = userInput.nextDouble();
-            System.out.println("Please enter the number of years employee 2 has worked: ");
-            employeesYears = userInput.nextDouble();
-            System.out.println("Employee one receives a 15% bonus, bringing their monthly pay to $" + newPay);
-            System.out.println("Please enter employee 3’s monthly pay: ");
-            currentPay = userInput.nextDouble();
-            System.out.println("Please enter the number of years employee 3 has worked: ");
-            employeesYears = userInput.nextDouble();
-            System.out.println("Employee one receives a 10% bonus, bringing their monthly pay to $" + newPay);
+
+            totalEmployees = userInput.nextInt();
+
+            for (int i = 0; i < totalEmployees; i++) {
+
+                int numEmployee[] = new int[totalEmployees];
+                ++numEmployee[i];
+
+                System.out.println("Please enter employee " + numEmployee[i] + "’s monthly pay: ");
+                currentPay = userInput.nextDouble();
+                System.out.println("Please enter the number of years employee " + numEmployee[i] + " has worked: ");
+                employeesYears = userInput.nextInt();
+
+                if (employeesYears >= 0 && employeesYears <= 4) {
+                    bonus = .05;
+                    bonus = currentPay * bonus;
+                    newPay = currentPay + bonus;
+                    System.out.println("Employee " + numEmployee[i]
+                            + " eceives a 5 % bonus, bringing their monthly pay to $" + newPay);
+
+                } else if (employeesYears >= 5 && employeesYears <= 9) {
+                    bonus = .10;
+                    bonus = currentPay * bonus;
+                    newPay = currentPay + bonus;
+                    System.out.println("Employee " + numEmployee[i]
+                            + " receives a 10% bonus, bringing their monthly pay to $" + newPay);
+
+                } else if (employeesYears >= 10) {
+                    bonus = .15;
+                    bonus = currentPay * bonus;
+                    newPay = currentPay + bonus;
+                    System.out.println("Employee " + numEmployee[i]
+                            + " receives a 15% bonus, bringing their monthly pay to $" + newPay);
+
+                } else {
+                    System.out.println("Invalid input.");
+                }
+            }
         } finally {
             userInput.close();
         }
