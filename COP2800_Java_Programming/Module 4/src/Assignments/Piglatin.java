ba19cfccaf55cd8360de0a330c7d6ee80f5592ed
Codetheist <56620151+Codetheist@users.noreply.github.com>
2021-03-10 12:36:51 -0500

    Update Piglatin.java

--- COP2800_Java_Programming/Module 4/src/Assignments/Piglatin.java
+++ COP2800_Java_Programming/Module 4/src/Assignments/Piglatin.java
@@ -1,11 +1,42 @@
+
 /****************************************************************************************************
  * Homework 4: Pig Latin Program By: Antoine Gustave File Name: Piglatin.java
  * Function: This program converts Strings to Pig Latin
  ******************************************************************************************************/
 
+import java.util.Scanner;
+
 public class Piglatin {
+    String originalPhrase;
+    String translatedPhrase;
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Piglatin newTranslation = new Translator();
+        newTranslation.translation();
+        newTranslation.displayInfo();
+    }
+
+    public Translator() {
+        originalPhrase = "";
+        translatedPhrase = "";
+    }
+
+    public void translation() {
+        Scanner userInput = new Scanner(System.in);
+
+        try {
+            System.out.println("Pig Latin Converter!");
+            System.out.println();
+
+            System.out.println("Please enter a phrase to be converted:");
+            originalPhrase = userInput.nextLine();
+
+        } finally {
+            userInput.close();
+        }
+    }
+
+    public void displayInfo() {
+        System.out.println("Your new phrase in Pig Latin:");
     }
 }
