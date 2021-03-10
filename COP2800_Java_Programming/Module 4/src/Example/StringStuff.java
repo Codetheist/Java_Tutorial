d49dd4e78438f7bb2410190e229d6db7f146a420
Codetheist <codetheist@codetheist.com>
2021-03-10 14:29:57 -0500

    Learning to covert Strings into other data type & use a GUI

--- COP2800_Java_Programming/Module 4/src/Example/StringStuff.java
+++ COP2800_Java_Programming/Module 4/src/Example/StringStuff.java
@@ -8,8 +8,55 @@ public class StringStuff {
 
         try {
 
-            String fullName = "Antoine Gustave";
-            String middle = "Denis";
+            // Learning how to split and cat strings
+            /*
+             * String fullName = "Antoine Gustave"; String middle = "Denis";
+             * 
+             * int spacePos = fullName.indexOf(' ');
+             * 
+             * String first = fullName.substring(0, spacePos); String last =
+             * fullName.substring(spacePos + 1);
+             * 
+             * System.out.println("Hello " + first + " " + last);
+             * 
+             * System.out.println();
+             * 
+             * char firstInitial = first.charAt(0); char lastInitial = last.charAt(0);
+             * 
+             * System.out.println("Please initial here: " + firstInitial + lastInitial);
+             * 
+             * System.out.println();
+             * 
+             * int nameLength = fullName.length();
+             * 
+             * System.out.println("Full name " + first + " " + middle + " " + last);
+             * 
+             * System.out.println();
+             * 
+             * System.out.println("Name length: " + nameLength);
+             */
+
+            // Learning to compare strings
+            /*
+             * String name1 = ""; String name2 = "";
+             * 
+             * System.out.println("Enter name #1: "); name1 = scan.next();
+             * System.out.println("Enter name #2: "); name2 = scan.next();
+             * 
+             * if (name1.equals(name2)) { System.out.println("Equal!"); } else {
+             * System.out.println("Not Equal!"); }
+             */
+
+            // Learning to covert Strings into other data type
+            String num1 = JOptionPane.showInputDialog("Enter your first input: ");
+            String num2 = JOptionPane.showInputDialog("Enter your second input: ");
+
+            int numOne = Integer.parseInt(num1);
+            int numTwo = Integer.parseInt(num2);
+
+            System.out.println(numOne + numTwo);
+
+            JOptionPane.showMessageDialog(null, (numOne + numTwo));
 
         } finally {
 
