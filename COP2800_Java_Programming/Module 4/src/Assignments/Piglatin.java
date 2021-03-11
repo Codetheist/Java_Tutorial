45c1ee4d7a88b0c577a5ed8877b3c47a3270704d
Codetheist <codetheist@codetheist.com>
2021-03-10 21:11:30 -0500

    PigLatin program partially translated the words.

--- COP2800_Java_Programming/Module 4/src/Assignments/Piglatin.java
+++ COP2800_Java_Programming/Module 4/src/Assignments/Piglatin.java
@@ -1,54 +1,69 @@
 
 /****************************************************************************************************
- * Homework 4: Pig Latin Program By: Antoine Gustave File Name: Piglatin.java
+ * Homework 4: Pig Latin Program By: Antoine Gustave File Name: PigLatin.java
  * Function: This program converts Strings to Pig Latin
  ******************************************************************************************************/
 
 import java.util.Scanner;
-//import javax.swing.JOptionPane;
-import java.util.Arrays;
 
-public class Piglatin {
+public class PigLatin {
+
+    String translatePhrase = "";
+    String originalPhrase = "";
 
     public static void main(String[] args) {
+        PigLatin pl = new PigLatin();
+        pl.origin();
+        pl.convert();
+        pl.translateDisplay();
+    }
+
+    public String origin() {
         Scanner userInput = new Scanner(System.in);
-        String originalPhrase = "";
-        int spacePos = originalPhrase.indexOf(" ");
-        final int ARR_SIZE = 10;
-        String arr[] = new String[ARR_SIZE];
 
         try {
             System.out.println("Pig Latin Converter!");
+
             System.out.println();
 
-            System.out.println("Please enter a phrase to be converted:");
+            System.out.println("Enter your phrase: ");
             originalPhrase = userInput.nextLine();
+            originalPhrase = originalPhrase.toLowerCase();
 
-            int i = 0;
-
-            while (originalPhrase.length() > 0) {
-
-                if (spacePos > 0) {
-                    arr[i] = originalPhrase.substring(0, spacePos);
-                    originalPhrase = originalPhrase.substring(spacePos + 1);
-
-                } else {
-
-                    if (originalPhrase.length() > 0) {
-                        arr[i] = originalPhrase.substring(0);
-                        originalPhrase = "";
-                    }
-                }
-                i++;
-            }
-
-            // Pig latin translation
+            return originalPhrase;
 
         } finally {
             userInput.close();
         }
-
-        // System.out.println("Initial word: " + originPhrase);
-        // System.out.println("Pig Latin translation: " + translatedPhrase);
     }
+
+    public String translation(String originalPhrase) {
+
+        if (originalPhrase.startsWith("a") || originalPhrase.startsWith("e") || originalPhrase.startsWith("i")
+                || originalPhrase.startsWith("o") || originalPhrase.startsWith("u")) {
+
+            translatePhrase = originalPhrase + "way";
+
+        } else {
+
+            translatePhrase = originalPhrase.substring(1) + originalPhrase.charAt(0) + "ay";
+        }
+
+        return translatePhrase;
+
+    }
+
+    public void convert() {
+        String[] storedWords = originalPhrase.split(" ");
+        for (int i = 0; i < storedWords.length; i++) {
+            translatePhrase += translation(storedWords[i] + " ");
+        }
+    }
+
+    public void translateDisplay() {
+
+        System.out.println("Your original input: " + originalPhrase);
+        System.out.println("Translated output: " + translatePhrase);
+    }
+
 }
